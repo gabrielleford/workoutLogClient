290d1183128fb73521051a60ccd2951f551b2b1d
Gabrielle Ford <gabrielledeva@gmail.com>
2021-11-18 16:36:10 -0500

    finished 10.8

--- src/workouts/WorkoutIndex.js
+++ src/workouts/WorkoutIndex.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "reactstrap";
 import WorkoutCreate from "./WorkoutCreate";
+import WorkoutTable from "./WorkoutTable";
 
 const WorkoutIndex = (props) => {
   const [workouts, setWorkouts] = useState([]);
 
   const fetchWorkouts = () => {
-    console.log(props.token);
     fetch("http://localhost:3000/log/", {
       method: "GET",
       headers: new Headers({
@@ -17,7 +17,6 @@ const WorkoutIndex = (props) => {
       .then((res) => res.json())
       .then((logData) => {
         setWorkouts(logData);
-        console.log(logData);
       });
   };
 
@@ -32,9 +31,7 @@ const WorkoutIndex = (props) => {
           <WorkoutCreate fetchWorkouts={fetchWorkouts} token={props.token} />
         </Col>
         <Col md="9">
-          <h2>
-            Log a workout to see a table. This will be added in later pages
-          </h2>
+          <WorkoutTable workouts={workouts} fetchWorkouts={fetchWorkouts} token={props.token} />
         </Col>
       </Row>
     </Container>
--- src/workouts/WorkoutTable.js
+++ src/workouts/WorkoutTable.js
@@ -2,6 +2,33 @@ import React from "react";
 import { Table, Button } from "reactstrap";
 
 const WorkoutTable = (props) => {
+    const workoutMapper = () => {
+        return props.workouts.map((workout, index) => {
+            return(
+                <tr key={index}>
+                    <th scope='row'>{workout.id}</th>
+                    <td>{workout.result}</td>
+                    <td>{workout.description}</td>
+                    <td>{workout.definition}</td>
+                    <td>
+                        <Button color='warning'>Update</Button>
+                        <Button color='danger' onClick={() => {deleteWorkout(workout)}}>Delete</Button>
+                    </td>
+                </tr>
+            );
+        });
+    };
+
+    const deleteWorkout = (workout) => {
+        fetch(`http://localhost:3000/log/delete/${workout.id}`, {
+          method: "DELETE",
+          headers: new Headers({
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${props.token}`,
+          }),
+        }).then(() => props.fetchWorkouts());
+    }
+
   return (
     <>
       <h3>Workout History</h3>
@@ -15,7 +42,9 @@ const WorkoutTable = (props) => {
             <th>Definition</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+            {workoutMapper()}
+        </tbody>
       </Table>
     </>
   );
